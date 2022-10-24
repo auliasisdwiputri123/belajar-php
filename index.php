9459f96844174a2fe2cc140df24f06d2489b2ba9
Aulia Sisdwiputri <aulia.sisdwiputri@gmail.com>
2022-10-24 10:55:28 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World! <br>";
+
+$nama = "Aulia";
+$umur = 19;
+//bisa pake tag html juga
+echo "Nama saya <strong> $nama </strong> , saya berusia $umur tahun. <br>";
+
+$namaAdik = "Tito";
+$umurAdik = 18;
+
+echo "Nama saya <strong> $namaAdik </strong> , saya berusia $umurAdik tahun. <br>";
+
+$selisihUmur = $umur-$umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun ";
+
+?>
